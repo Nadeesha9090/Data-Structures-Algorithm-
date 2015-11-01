8ec53ed048d13c94944baacfde3406ba41de40fc
Nadeesha Thilakaraathne <nadeesha9090@gmail.com>
2015-11-01 19:19:45 +0530

    Add Project

---
+++ src/ArrayImplementations/FindMaximum.java
@@ -0,0 +1,16 @@
+package ArrayImplementations;
+
+/**
+ * Created by hp on 11/1/2015.
+ */
+public class FindMaximum {
+    int[] a = { 1, 1, 2, 3, 5, 8 };
+
+    int max = a[0];
+    int i =0;
+
+    for(i=0;i<=5;i++){
+
+
+    }
+}
